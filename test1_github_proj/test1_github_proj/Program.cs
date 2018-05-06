acb753747ccd5b0e0035b6db929e025f1682535d
Max <max.vitvitckiy@gmail.com>
2018-05-06 17:34:21 +0300

    cw world

--- test1_github_proj/test1_github_proj/Program.cs
+++ test1_github_proj/test1_github_proj/Program.cs
@@ -11,7 +11,7 @@ namespace test1_github_proj
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
-
+            Console.WriteLine("World");
             Console.ReadLine();
         }
     }
